1f44015777f976d8613686db8426796a1e5f695d
XIE Qianyue <xie.qianyue@gmail.com>
2015-03-22 23:27:35 +0100

    add a test to testBrowser

--- testBrowser/tests.js
+++ testBrowser/tests.js
@@ -3,4 +3,8 @@ describe('simple test', function() {
 	it('should equal 0 when n === 0', function() {
 		window.square(0).should.equal(0);
 	});
+
+	it('should equal 4 when n === 2', function() {
+		window.square(2).should.equal(4);
+	});
 });
