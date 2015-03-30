2cb269c8e8952a0a6b2698489823b144ec42b82f
xie-qianyue <xie.qianyue@gmail.com>
2015-03-30 17:55:21 +0200

    first commit

---
+++ express_angular1.3/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+// to solve the F5/Refresh problem
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000);
